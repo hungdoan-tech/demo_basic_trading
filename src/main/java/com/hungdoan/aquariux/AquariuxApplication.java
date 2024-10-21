5499b843139434397335344cd5b20bcb22a7055f
hungdoan-tech <hungdoan426@gmail.com>
2024-10-21 22:53:23 +0700

    Set timezone of web server and db server to UTC

--- src/main/java/com/hungdoan/aquariux/AquariuxApplication.java
+++ src/main/java/com/hungdoan/aquariux/AquariuxApplication.java
@@ -1,13 +1,20 @@
 package com.hungdoan.aquariux;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class AquariuxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AquariuxApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AquariuxApplication.class, args);
+    }
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
